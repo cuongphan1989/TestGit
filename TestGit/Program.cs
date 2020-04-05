2d5f62b05b3673025d704015e0f3cca7a79a5fe7
Andy <solomoon1003@gmail.com>
2020-04-05 17:04:24 +0700

    test reset

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -6,7 +6,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World1!");
             Console.WriteLine("ChangeFromMaster");
         }
     }
